41854a5c80e2eaaba4744557f226072cafe1a015
Kai Lazell <kai.lazell@outlook.com>
2024-09-08 14:10:43 +0100

    absolute pain

--- hot-beans-.js
+++ hot-beans-.js
@@ -3,3 +3,18 @@ document.getElementById('contactForm').addEventListener('submit', function(e) {
     document.getElementById('formMessage').textContent = "Thank you for your message!";
     this.reset();
 });
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
